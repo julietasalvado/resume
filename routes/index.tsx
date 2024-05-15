8721764ced6e17a5930c3c9d9ae863e89a8d00af
Julieta Salvadó <julieta.salvado@gmail.com>
2024-05-15 00:19:57 -0300

    Scroll Down Badge

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,11 @@
 import ScreenSize from "../islands/ScreenSize.tsx";
+import { CloudBadge } from "../components/CloudBadge.tsx";
 
 export default function Home() {
   return (
     <section className="bg-[#FDE3E9] h-screen">
       <ScreenSize />
+      <CloudBadge />
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,23 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Outfit", "sans-serif"],
+      },
+      keyframes: {
+        rotate: {
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+        rotateReverse: {
+          to: {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+    },
+  },
 } satisfies Config;
